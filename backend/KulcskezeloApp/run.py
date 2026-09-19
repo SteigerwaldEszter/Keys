0d0e83fc18e86f733771b67fa6d87525802450ed
Balla Zsófia <zsofiaballa7@gmail.com>
2026-09-19 12:46:49 +0200

    formatting

--- backend/KulcskezeloApp/run.py
+++ backend/KulcskezeloApp/run.py
@@ -10,16 +10,18 @@ app = Flask(__name__)
 wsgi_app = app.wsgi_app
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     """Renders a sample page."""
     return "Hello World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
+
+    HOST = os.environ.get("SERVER_HOST", "localhost")
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get("SERVER_PORT", "5555"))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
