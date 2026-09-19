5382fe7c91221faa6ddb1124f5b1ed5a1d5ddaef
Balla Zsófia <zsofiaballa7@gmail.com>
2026-09-19 12:54:15 +0200

    fix

--- backend/KulcskezeloApp/run.py
+++ backend/KulcskezeloApp/run.py
@@ -2,6 +2,7 @@
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
+
 from flask import Flask
 
 app = Flask(__name__)
