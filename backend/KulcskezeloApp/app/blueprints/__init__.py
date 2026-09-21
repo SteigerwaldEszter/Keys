9fbe17e9b91277f73c214a6d6b5b2b191bad6b3f
SteigerwaldEszter <stejgeszter@gmail.com>
2026-09-21 19:27:18 +0200

    fix: formázás megint

--- backend/KulcskezeloApp/app/blueprints/__init__.py
+++ backend/KulcskezeloApp/app/blueprints/__init__.py
@@ -1,4 +1,4 @@
-﻿# from datetime import datetime
+# from datetime import datetime
 # from functools import wraps
 
 # from apiflask import apiblueprint, httperror
--- backend/KulcskezeloApp/app/extensions.py
+++ backend/KulcskezeloApp/app/extensions.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import DeclarativeBase
 class Base(DeclarativeBase):
     pass
 
-db = SQLAlchemy(model_class = Base)
 
+db = SQLAlchemy(model_class=Base)
 
 
 auth = HTTPTokenAuth()
